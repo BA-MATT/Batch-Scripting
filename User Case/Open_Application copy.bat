62bead407ffa4ed449205d6f67d7296465dcc070
Sumit Mondal <161226094+BA-MATT@users.noreply.github.com>
2024-08-26 21:17:08 +0530

    Add files via upload

---
+++ User Case/Open_Application copy.bat
@@ -0,0 +1,13 @@
+@echo off
+
+Title Open_APPS
+echo open Application via .bat file
+pause
+
+REM Check the .exe location of the file location 
+
+start /d "C:\Program Files\Microsoft Office\root\Office16" ONENOTE.EXE
+start /d "C:\Users\hanus\AppData\Local\Programs\Microsoft VS Code" Code.exe
+
+
+
---
+++ User Case/Open_URL Incognito.bat
@@ -0,0 +1,10 @@
+@echo off
+
+Title Open_URL
+echo open daily Open_URL in Incognito or InPrivate
+pause
+
+start chrome.exe -incognito https://www.youtube.com/
+start msedge.exe inprivate https://www.google.com
+
+
